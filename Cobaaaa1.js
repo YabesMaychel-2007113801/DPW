b44798981c447f4c3db0df44ecac19b0348af961
Yabesmaych <yabesmaych@gmail.com>
2022-03-15 17:21:02 +0700

    first commit

---
+++ Cobaaaa1.js
@@ -0,0 +1,10 @@
+console.log("Prak4");
+
+const hem = document.getElementsByTagName("h1");
+hem[0].style.color = "blue";
+
+const au = document.getElementsByClassName("bawah");
+au[0].style.color ="red";
+
+const xixi = document.getElementsById("Eti");
+xixi.style.color = "pink";
---
+++ Script/script.js
@@ -0,0 +1,3 @@
+console.log("Hadeh");
+const hem = document.getElementsByClassName("pecel");
+hem[0].style.color = "red";
